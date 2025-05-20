04a9a83b8d9e8e849176b9726706a2c7fa1b54a6
Mohammednur <mamebb2023@gmail.com>
2025-05-20 15:57:27 +0300

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/home/Hero";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="min-h-screen overflow-x-hidden bg-gray-50">
+      <Hero />
+    </div>
+  );
+};
+
+export default Page;
