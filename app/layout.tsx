af04abac2683d1d0f3e4cfd978ea98e7d0def907
Mohammednur <mamebb2023@gmail.com>
2025-07-05 14:24:34 +0300

    Update layout component for improved structure and performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,30 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import ReactLenis from "lenis/react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Mohammednur | Software Engineer",
   description:
     "Portfolio of Mohammednur, a software engineer specializing in web development.",
+  keywords: [],
+  formatDetection: {
+    telephone: false,
+    email: false,
+    address: false,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  icons: {
+    icon: [{ url: "/favicon.png", sizes: "any" }],
+    shortcut: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +34,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PCHSN4M2');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`font-cabin-variable antialiased overflow-x-hidden`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PCHSN4M2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ReactLenis root>{children}</ReactLenis>
       </body>
     </html>
